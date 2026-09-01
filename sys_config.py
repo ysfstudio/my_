19bf298b7e307253d16effb7029a87d3096fe904
ysfstudio <yousefomarali77@gmail.com>
2026-09-01 07:57:06 +0300

    Fix reference to current file in BASE_DIR

--- sys_config.py
+++ sys_config.py
@@ -1,6 +1,6 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.abspath(file))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 DB_PATH = os.path.join(DATA_DIR, "logs.db")
 
